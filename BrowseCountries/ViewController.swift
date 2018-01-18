1b2708eb74bda25f8d3d86262ff1752327ebf0f7
Alberto Vega <Alberto-Vega@users.noreply.github.com>
2018-01-17 16:10:05 -0800

    Successfully got data from url

--- BrowseCountries/ViewController.swift
+++ BrowseCountries/ViewController.swift
@@ -12,9 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let stringURL = "https://restcountries-v1.p.mashape.com/all"
+        guard let url = URL(string: stringURL) else {
+            print("Log Error: unable to create URL from string")
+            return
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("1IosQYQKu0mshuIZjcqiIXbiLGJSp1dBB9Yjsnfd2aISWLA7Yk", forHTTPHeaderField: "X-Mashape-Key")
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Accept")
+        let session = URLSession.shared
+        let task = session.dataTask(with: urlRequest) { (data, response, error) in
+            print(response)
+        }
+        task.resume()
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
